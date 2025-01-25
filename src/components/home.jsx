9e7b1a1bda54038d4d01eb70441a11f2bab76fba
Diptanshu Dey <diptanshudey4@gmail.com>
2025-01-25 15:11:18 +0530

    Added new links

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,22 +12,22 @@ function home() {
                 <p>Hey there! <br/>These are our social media profiles. Hope you like it</p>
                 <div className='social-links' id='insta'>
                     <i class='bx bxl-instagram-alt'></i>
-                    <a href='https://www.instagram.com/srtglobalsolutions/?utm_source=ig_web_button_share_sheet'>Instagram</a>
+                    <a href='https://www.instagram.com/koadai_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='>Instagram</a>
                 </div>
                 <div className='social-links' id='facebook'>
                     <i class='bx bxl-facebook-circle'></i>
-                    <a href='https://www.facebook.com/people/srtglobalsolutions/61555885036510/?mibextid=ZbWKwL'>Facebook</a>
+                    <a href='https://www.facebook.com/people/Koadai/61570458852555/'>Facebook</a>
                     </div>
                 <div className='social-links' id='linkedIn'>
                     <i class='bx bxl-linkedin-square' ></i>
-                    <a href='www.linkedin.com/in/srt-global-solutions-319361340'>LinkedIn</a></div>
+                    <a href='https://in.linkedin.com/company/koadai'>LinkedIn</a></div>
                 <div className='social-links' id='pinterest'>
                     <i class='bx bxl-pinterest' ></i>
                     <a href='https://in.pinterest.com/srtglobalsolutions/?invite_code=362c552e7cb140b28d2aec9cc0b06c35&sender=986429262041124269'>Pinterest</a>
                     </div>
                 <div className='social-links' id='website'>
                     <i class='bx bx-globe-alt' ></i>
-                    <a href='https://srtglobalsolutions.com/'>Website</a>
+                    <a href='https://koadai.com/'>Website</a>
                     </div>
                     <button className='mt-5' id='btn'><a href='tel: +917043661691'>Get in touch</a></button>
                     <p id='thank-p' className='pb-5'>Thank you! For your interest</p>
